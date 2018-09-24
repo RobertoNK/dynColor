5bb7effc17f3ae54f3e8a200236afb4b06e61c21
RobertoNK <roberto.dg2611@gmail.com>
2018-09-24 15:36:17 -0400

    committing html and javascrip files

---
+++ dynColors.js
@@ -0,0 +1,9 @@
+function setColor(where, newColor){
+    if(where == "background"){
+        document.body.style.backgroundColor = newColor;
+    }
+    else{
+        document.body.style.color = newColor;
+    }
+
+}
